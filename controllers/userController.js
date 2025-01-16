734106c2f0ff493ed9d9569aa13553f51fdd23ab
Jakaria030 <gulamjakaria99@gmail.com>
2025-01-16 11:15:42 +0600

    User get api updated for query

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,14 +4,34 @@ const jwt = require('jsonwebtoken');
 exports.postJWT = async (req, res) => {
     const user = req.body;
     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-    
+
     res.send({ token });
 };
 
 exports.getUser = async (req, res) => {
-    const {userCollections} = getCollections();
+    const { userCollections } = getCollections();
 
-    const result = await userCollections.find().toArray();
+    const search = req.query.search || "";
+    const filter = req.query.filter;
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const skip = (page-1)*limit;
+
+    const query = {};
+
+    if (search) {
+        query.$or = [
+            { name: { $regex: search, $options: "i" } }, 
+            { email: { $regex: search, $options: "i" } },
+        ];
+    }
+
+    if (filter) {
+        query.role = filter;
+    }
+
+    const result = await userCollections.find(query).skip(skip).limit(limit).toArray();
 
     res.send(result);
 };
@@ -36,12 +56,12 @@ exports.isExistUser = async (req, res) => {
 };
 
 exports.userRole = async (req, res) => {
-    const {userCollections} = getCollections();
+    const { userCollections } = getCollections();
 
     const email = req.query.email;
-    const query = {email: email};
+    const query = { email: email };
     const options = {
-        projection: {role: 1, _id: 0}
+        projection: { role: 1, _id: 0 }
     };
 
     const result = await userCollections.findOne(query, options);
