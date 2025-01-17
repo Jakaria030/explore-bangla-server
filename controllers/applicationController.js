486153195f52be2d5529cf15b84c44666d127af1
Jakaria030 <gulamjakaria99@gmail.com>
2025-01-17 18:20:19 +0600

    Get api added for get all applicants

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -1,10 +1,62 @@
 const { getCollections } = require("../dbConfig/dbConfig");
 
 exports.postApplication = async (req, res) => {
-    const {applicationCollections} = getCollections();
+    const { applicationCollections } = getCollections();
 
     const application = req.body;
     const result = await applicationCollections.insertOne(application);
+
+    res.send(result);
+};
+
+exports.getApplications = async (req, res) => {
+    const { applicationCollections } = getCollections();
+
+
+
+    if (req.user.email !== req.query.email) {
+        return res.status(403).json({ message: "Access denied. Admins only." });
+    }
+
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+
+    const result = await applicationCollections.aggregate([
+        {
+            $lookup: {
+                from: "users",
+                localField: "email",
+                foreignField: "email",
+                as: "userDetails"
+            }
+        },
+        {
+            $unwind: {
+                path: "$userDetails",
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $project: {
+                _id: 1,
+                name: "$userDetails.name",
+                image: "$userDetails.image",
+                email: "$email",
+                role: "$userDetails.role",
+                title: 1,
+                reason: 1,
+                cvLink: 1
+            }
+        },
+        {
+            $skip: skip
+        },
+        {
+            $limit: limit
+        }
+    ]).toArray();
 
     res.send(result);
 };
--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { verifyToken, verifyTourist } = require('../middlewares/authMiddlewares');
-const { postApplication } = require('../controllers/applicationController');
+const { verifyToken, verifyTourist, verifyAdmin } = require('../middlewares/authMiddlewares');
+const { postApplication, getApplications } = require('../controllers/applicationController');
 const router = express.Router();
 
 router.post('/applications', verifyToken, verifyTourist, postApplication);
+router.get('/applications', verifyToken, verifyAdmin, getApplications);
 
 module.exports = router;
